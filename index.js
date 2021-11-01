2871e8a45f70a0baa43a95e1b1b73bcd97d97b7e
aaronjenkins <aaron@aaronjenkins.me>
2021-11-01 12:00:12 -0400

    add SerpAPI code

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+const SerpApi = require('google-search-results-nodejs')
+const search = new SerpApi.GoogleSearch(process.env.SerpAPIKey)
+
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Twitter bot!'),
-    };
-    return response;
+    const params = {
+        q: "events in Cincinnati, OH",
+        google_domain: "google.com",
+        gl: "us",
+        hl: "en"
+      };
+      
+      const callback = function(data) {
+        console.log(data['events_results']);
+      };
+      
+    return search.json(params, callback);
 };
 
