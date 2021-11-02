dbf50d0488b328cc26a2eb29e90332168aae047b
aaronjenkins <aaron@aaronjenkins.me>
2021-11-01 22:58:39 -0400

    fix index.js formatting

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 const axios = require("axios");
-const {TwitterApi} = require("twitter-api-v2");
+const { TwitterApi } = require("twitter-api-v2");
 
 exports.handler = async (event) => {
-    const params = {
-        api_key: process.env.SerpAPIKey,
-        q: "events in Cincinnati, OH",
-        google_domain: "google.com",
-        gl: "us",
-        hl: "en"
-      };
-      
-      let response = await axios.get("https://serpapi.com/search", {params: params})
-      let localevents = response.data.events_results
-      var now = new Date();
-      const todayFormatted = now.toLocaleString('en-US', {  month: 'short', day: 'numeric' });
+  const params = {
+    api_key: process.env.SerpAPIKey,
+    q: "events in Cincinnati, OH",
+    google_domain: "google.com",
+    gl: "us",
+    hl: "en",
+  };
 
-      const tomorrow = new Date(now)
-      tomorrow.setDate(tomorrow.getDate() + 1)
-      const tomorrowFormatted = tomorrow.toLocaleString('en-US', {  month: 'short', day: 'numeric' });
+  let response = await axios.get("https://serpapi.com/search", {
+    params: params,
+  });
+  let localevents = response.data.events_results;
+  let now = new Date();
+  let todayFormatted = now.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
 
+  let tomorrow = new Date(now);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  let tomorrowFormatted = tomorrow.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
 
-      const dayAfterTomorrow = new Date(now)
-      dayAfterTomorrow.setDate(now.getDate() + 2)
-      const dayAfterTomorrowFormatted = dayAfterTomorrow.toLocaleString('en-US', {  month: 'short', day: 'numeric' });
+  let dayAfterTomorrow = new Date(now);
+  dayAfterTomorrow.setDate(now.getDate() + 2);
+  let dayAfterTomorrowFormatted = dayAfterTomorrow.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
 
-      let nextThreeDays = [todayFormatted, tomorrowFormatted, dayAfterTomorrowFormatted ];
-      let eventsInTheNextThreeDays =  localevents.filter(function(item) {
-        return nextThreeDays.indexOf(item.date.start_date) !== -1;
-      });
-      
-      eventsInTheNextThreeDays.sort ( function (a, b){
-             return new Date(a.date.start_date) - new Date(b.date.start_date);
-      });
-      
-      let tweet = "Upcoming Events: \r\n";
-      for (let i = 0; i < eventsInTheNextThreeDays.length; i++) {
-        tweet += `\r\n${eventsInTheNextThreeDays[i].title}\r\n${eventsInTheNextThreeDays[i].date.when}\r\nat ${eventsInTheNextThreeDays[i].address[0]}\r\n`;
-      }
+  let nextThreeDays = [
+    todayFormatted,
+    tomorrowFormatted,
+    dayAfterTomorrowFormatted,
+  ];
+  let eventsInTheNextThreeDays = localevents.filter(function (item) {
+    return nextThreeDays.indexOf(item.date.start_date) !== -1;
+  });
 
-      var client = new TwitterApi({
-          appKey: process.env.TWITTER_CONSUMER_KEY,
-          appSecret: process.env.TWITTER_CONSUMER_SECRET,
-          accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
-          accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-      })
+  eventsInTheNextThreeDays.sort(function (a, b) {
+    return new Date(a.date.start_date) - new Date(b.date.start_date);
+  });
 
-      await client.v1.tweet(tweet);
+  let tweet = "Upcoming Events: \r\n";
+  for (let i = 0; i < eventsInTheNextThreeDays.length; i++) {
+    tweet += `\r\n${eventsInTheNextThreeDays[i].title}\r\n${eventsInTheNextThreeDays[i].date.when}\r\nat ${eventsInTheNextThreeDays[i].address[0]}\r\n`;
+  }
 
-      return tweet;
+  var client = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  await client.v1.tweet(tweet);
+
+  return tweet;
 };
-
