f6622231f8a1dd536cb8469a01188d328027adc1
aaronjenkins <aaron@aaronjenkins.me>
2021-11-05 08:51:16 -0400

    clean up js, add 0 event handling

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
-const axios = require("axios");
-const { TwitterApi } = require("twitter-api-v2");
+const axios = require('axios');
+const { TwitterApi } = require('twitter-api-v2');
 
 exports.handler = async (event) => {
-  const params = {
-    api_key: process.env.SerpAPIKey,
-    q: "Events in Cincinnati, OH",
-    google_domain: "google.com",
-    gl: "us",
-    hl: "en",
-  };
+    let response = await axios.get('https://serpapi.com/search', {
+        params: {
+            api_key: process.env.SerpAPIKey,
+            q: 'Events in Cincinnati, OH',
+            google_domain: 'google.com',
+            gl: 'us',
+            hl: 'en',
+        },
+    });
 
-  let response = await axios.get("https://serpapi.com/search", {
-    params: params,
-  });
-  let now = new Date();
-  let todayFormatted = now.toLocaleString("en-US", {
-    month: "short",
-    day: "numeric",
-  });
+    let eventsInTheNextThreeDays;
+    let tweet;
 
-  let tomorrow = new Date(now);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  let tomorrowFormatted = tomorrow.toLocaleString("en-US", {
-    month: "short",
-    day: "numeric",
-  });
+    if (response.data.events_results.count > 0) {
+        let now = new Date();
+        let todayFormatted = now.toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+        });
 
-  let dayAfterTomorrow = new Date(now);
-  dayAfterTomorrow.setDate(now.getDate() + 2);
-  let dayAfterTomorrowFormatted = dayAfterTomorrow.toLocaleString("en-US", {
-    month: "short",
-    day: "numeric",
-  });
+        let tomorrow = new Date(now);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        let tomorrowFormatted = tomorrow.toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+        });
 
-  let nextThreeDays = [
-    todayFormatted,
-    tomorrowFormatted,
-    dayAfterTomorrowFormatted,
-  ];
-  let eventsInTheNextThreeDays = response.data.events_results.filter(function (item) {
-    return nextThreeDays.indexOf(item.date.start_date) !== -1;
-  });
+        let dayAfterTomorrow = new Date(now);
+        dayAfterTomorrow.setDate(now.getDate() + 2);
+        let dayAfterTomorrowFormatted = dayAfterTomorrow.toLocaleString(
+            'en-US',
+            {
+                month: 'short',
+                day: 'numeric',
+            }
+        );
 
-  eventsInTheNextThreeDays.sort(function (a, b) {
-    return new Date(a.date.start_date) - new Date(b.date.start_date);
-  });
+        let nextThreeDays = [
+            todayFormatted,
+            tomorrowFormatted,
+            dayAfterTomorrowFormatted,
+        ];
+        eventsInTheNextThreeDays = response.data.events_results.filter(
+            function (item) {
+                return nextThreeDays.indexOf(item.date.start_date) !== -1;
+            }
+        );
 
-  let tweet = "Upcoming Events: \r\n";
-  for (let i = 0; i < eventsInTheNextThreeDays.length; i++) {
-    tweet += `\r\n${eventsInTheNextThreeDays[i].title}\r\n${eventsInTheNextThreeDays[i].date.when}\r\nat ${eventsInTheNextThreeDays[i].address[0]}\r\n`;
-  }
+        eventsInTheNextThreeDays.sort(function (a, b) {
+            return new Date(a.date.start_date) - new Date(b.date.start_date);
+        });
 
-  var client = new TwitterApi({
-    appKey: process.env.TWITTER_CONSUMER_KEY,
-    appSecret: process.env.TWITTER_CONSUMER_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  });
+        tweet += 'Upcoming Events: \r\n';
+        for (let i = 0; i < eventsInTheNextThreeDays.length; i++) {
+            tweet += `\r\n${eventsInTheNextThreeDays[i].title}\r\n${eventsInTheNextThreeDays[i].date.when}\r\nat ${eventsInTheNextThreeDays[i].address[0]}\r\n`;
+        }
+    } else {
+        tweet = "weird, didn't find any events for the next 3 days...";
+    }
 
-  await client.v1.tweet(tweet);
+    var client = new TwitterApi({
+        appKey: process.env.TWITTER_CONSUMER_KEY,
+        appSecret: process.env.TWITTER_CONSUMER_SECRET,
+        accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
 
-  return tweet;
+    await client.v1.tweet(tweet);
+
+    return tweet;
 };
