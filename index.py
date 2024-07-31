c9ee9cfd2a41069fc9bc648f8ab0bbe9532b593e
aaronjenkins <aaron@aaronjenkins.me>
2024-07-30 21:28:21 -0400

    use serpapi package instead of direct http call

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import os
-import requests
 from datetime import datetime, timedelta
+from serpapi import GoogleSearch
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()  # Load environment variables
 
 def get_upcoming_events():
     api_key = os.environ.get('serp_api_key')
-    endpoint = 'https://serpapi.com/search'
     params = {
         'api_key': api_key,
         'q': 'Events in Cincinnati, OH',
@@ -17,24 +17,17 @@ def get_upcoming_events():
         'engine': 'google_events'
     }
 
-    try:
-        response = requests.get(endpoint, params=params)
-        response.raise_for_status()
-        print(f"Response: {response.json()}")
-        return response.json().get('events_results')
-    except requests.RequestException as error:
-        print(f"Failed to fetch events: {error}")
-        return None
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    print(f"Response: {results}")
+    return results.get('events_results')
 
 def is_event_in_the_next_three_days(event_date):
     today = datetime.now()
-    # Try parsing the date with the new format
     try:
         event_dt = datetime.strptime(event_date, "%b %d")
-        # Set the correct year for the event
         event_dt = event_dt.replace(year=today.year)
     except ValueError:
-        # If parsing fails, print the error and return False
         print(f"Error parsing date: {event_date}")
         return False
     
@@ -64,18 +57,15 @@ def send_message(message):
     except requests.RequestException as error:
         print(f"Error: {error}")
         print("Response Status Code:", response.status_code)
-        print("Response Body:", response.text)  
+        print("Response Body:", response.text)
 
 def parse_event_date(event_date):
     today = datetime.now()
     try:
-        # Parse the date with the format 'Nov 3'
         event_dt = datetime.strptime(event_date, "%b %d")
-        # Set the year to the current year
         event_dt = event_dt.replace(year=today.year)
         return event_dt
     except ValueError:
-        # Log the error for debugging
         print(f"Error parsing date: {event_date}")
         return None
 
@@ -86,7 +76,6 @@ def main():
             item for item in events_results if is_event_in_the_next_three_days(item['date']['start_date'])
         ]
         
-        # Sort events based on the new date parsing function
         events_in_the_next_three_days.sort(
             key=lambda x: parse_event_date(x['date']['start_date']) or datetime.min
         )
@@ -95,11 +84,11 @@ def main():
         for event in events_in_the_next_three_days:
             message += f"\n{event['title']}\n{event['date']['when']}\nat {event['address'][0]}\n"
         
-        send_message(message)
+        # send_message(message)
         print('Processed events successfully.')
     else:
         message = "weird, didn't find any events for the next 3 days..."
-        send_message(message)
+        # send_message(message)
         print('weird, didn\'t find any events for the next 3 days...')
 
 if __name__ == "__main__":
